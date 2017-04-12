9a471cc7b089daf715634820d1652e8add5d64ff
Wyatt Robinson <thewyatt@gmail.com>
2017-04-12 23:13:10 +0000

    Added links to view pages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
-class ArticlesController <
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
   def show
     @article = Article.find(params[:id])
   end
